d75409ca7703e3e7e9ef8f4574fed56550f027be
tetsu5555 <tetete1118@gmail.com>
2019-12-31 02:20:53 +0900

    add

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
+const add = (a: number, b: number): number => a + b;
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log(add(5, 5));
     res.send('Hello');
 });
 
