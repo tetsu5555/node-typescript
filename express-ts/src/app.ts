a5dcdb6b1160e0fba0be461c6d8beb7f768bf97b
tetsu5555 <tetete1118@gmail.com>
2019-12-31 23:55:25 +0900

    add

--- express-ts/src/app.ts
+++ express-ts/src/app.ts
@@ -7,7 +7,7 @@ const add = (a: number, b: number): number => a + b;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     console.log(add(5, 5));
-    res.send('Hello');
+    res.send('Hello!');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
